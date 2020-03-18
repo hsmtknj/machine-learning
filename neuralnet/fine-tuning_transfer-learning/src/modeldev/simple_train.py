00a74080f23e10ac58b62f7c50c06985533e7c32
hsmtknj <emoke.any@gmail.com>
2020-03-18 21:54:04 +0900

    add todo list to simple_train.py

--- neuralnet/fine-tuning_transfer-learning/src/modeldev/simple_train.py
+++ neuralnet/fine-tuning_transfer-learning/src/modeldev/simple_train.py
@@ -2,6 +2,7 @@
 Implement simple training
 """
 
+#* Import Libraries *#
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -10,7 +11,7 @@ from sklearn.utils import shuffle
 from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 
-# register src directory path to PYTHONPATH
+#* Register src directory path to PYTHONPATH *#
 import sys
 from os import path, pardir
 current_dir = path.abspath(path.dirname(__file__))
@@ -18,41 +19,62 @@ parent_dir = path.abspath(path.join(current_dir, pardir))
 parent_parent_dir = path.abspath(path.join(parent_dir, pardir))
 sys.path.append(parent_dir)
 
+#* Import my Libraries *#
 from mylib import utils
 from modeldev import d_layer
 from modeldev import d_model
 from modeldev import d_loss_func
 from modeldev import d_calc_loss_func
 
+#* Set params *#
+# general params
 random_state = 50
 test_size_ratio = 0.1
 valid_size_ratio = 0.1
 
+# training params
+vae_layer_dim_list = [13, 7, 5]
+vae_layer_type_list = [d_layer.Dense, d_layer.Drop]
+vae_act_func_list = [tf.nn.relu, tf.nn.relu6]
+vae_loss_func = tf.losses.mean_squared_error
+vae_optimizer = tf.train.AdamOptimizer
+vae_epochs = 10
+vae_kld_coef = 1.0
+mlp_layer_dim_list = [3, 1]
+mlp_layer_type_list = [d_layer.Dense, d_layer.Dense]
+mlp_act_func_list = [tf.nn.relu, tf.nn.sigmoid]
+mlp_loss_func = tf.losses.mean_squared_error
+mlp_optimizer = tf.train.AdamOptimizer
+mlp_epochs = 10
 
-if __name__ == '__main__':
-    # load data and split into train, test and valid data
+
+def simple_train_main():
+
+    # load data
     dirname = parent_parent_dir + '/data/dataset'
     data_X, data_y = utils.load_dataset_X_y(dirname)
+
+    # ======================================================================= #
+    # Data Preprocesing
+    # ======================================================================= #
+
+    # TODO: data preprocessing for all data
+
+    # split into train, test and valid data
     train_X, test_X, valid_X, train_y, test_y, valid_y = utils.train_test_valid_split(data_X,
                                                                                       data_y,
                                                                                       test_size=test_size_ratio,
                                                                                       valid_size=valid_size_ratio,
                                                                                       random_state=random_state)
 
-    # set param
-    vae_layer_dim_list = [13, 7, 5]
-    vae_layer_type_list = [d_layer.Dense, d_layer.Drop]
-    vae_act_func_list = [tf.nn.relu, tf.nn.relu6]
-    vae_loss_func = tf.losses.mean_squared_error
-    vae_optimizer = tf.train.AdamOptimizer
-    vae_epochs = 10
-    vae_kld_coef = 1.0
-    mlp_layer_dim_list = [3, 1]
-    mlp_layer_type_list = [d_layer.Dense, d_layer.Dense]
-    mlp_act_func_list = [tf.nn.relu, tf.nn.sigmoid]
-    mlp_loss_func = tf.losses.mean_squared_error
-    mlp_optimizer = tf.train.AdamOptimizer
-    mlp_epochs = 10
+    # TODO: data preprocessing for split data
+
+    # TODO: data preprocessing for train data
+
+
+    # ======================================================================= #
+    # Training
+    # ======================================================================= #
 
     # define model
     model = d_model.Model(vae_layer_dim_list,
@@ -79,3 +101,7 @@ if __name__ == '__main__':
     print('ERROR:{}'.format(error))
 
     print('END')
+
+
+if __name__ == '__main__':
+    simple_train_main()
